6a259502683315b668f793b18f010e617a39e688
Seiya-Umemoto <seiya.u77@gmail.com>
2020-03-08 15:29:23 +0900

    changed directory structures

--- MyBlog/mysite/sqlite3_table_check.R
+++
@@ -1,9 +0,0 @@
-install.packages("RSQLite") #perhaps needed
-library("RSQLite")
-library()
-
-# connect to the sqlite file
-sqlite    <- dbDriver("SQLite")
-exampledb <- dbConnect(sqlite,"db.sqlite3")
-
-dbListTables(exampledb)
